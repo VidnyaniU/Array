44ebf76fe0296decbadcb2177e39f02f0f0e89b6
VidnyaniU <150503254+VidnyaniU@users.noreply.github.com>
2023-11-22 09:07:18 +0530

    Update removeduplicates.c

--- removeduplicates.c
+++ removeduplicates.c
@@ -1,15 +1,31 @@
-
 #include <stdio.h>
+
+int removeDuplicates(int arr[], int size)
+{
+		if (n == 0 || n == 1) //empty array or single element in the array
+		return n;
+
+	int temp[n]; //temporary array
+  int j = 0;
+  
+  for (int i = 0; i < n - 1; i++)
+		if (arr[i] != arr[i + 1])
+			temp[j++] = arr[i];
+  temp[j++] = arr[n - 1];
+
+	for (int i = 0; i < j; i++) //copying elements to the original array
+		arr[i] = temp[i];
+
+	return j;
+}
+
 int main()
 {
-int sortedArr[]={1,2,2,4,7,7};
-int size= sizeof(arr)/sizeof(arr[0]);
-for (int i=0;i<size;i++)
-{
-if(sortedArr[i]==sortedArr [i+1])
-for (int j=0;j<size;j++)
-{sortedArr [j+1]=sortedArr [j+2];
-}
-}
-return 0;
+	int arr[] = { 2,2,4,5,6,6 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+  removeDuplicates(arr, size);
+
+	for (int i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+	return 0;
 }
